ebb0dad29f26357126d66adda54f0a2174b70543
wadsahin <wadsahinalom@gmail.com>
2025-05-03 03:53:22 -0700

    Mongodb connected with server successfully

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 
-// middleware 
+// middleware
 app.use(cors());
 app.use(express.json());
+
+
+// CoffeeStore
+// M57sD3mu0JwFYYKB
+
+
+// const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.l0f7v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://CoffeeStore:M57sD3mu0JwFYYKB@cluster0.l0f7v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res)=>{
